d1d648dcb4746b88d2711042671fc83d060a4a10
DOMINGOS CAPEWA <domingos.capewa@senacsp.edu.br>
2025-08-01 22:17:34 -0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OlaMundo } from "./components/ola-mundo/ola-mundo";
+import { Perfil } from './components/perfil/perfil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OlaMundo, Perfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
